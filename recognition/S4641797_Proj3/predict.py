f264fcd33d5926a3f1b07de7e26bd66506657455
IHopeThisIsNotPermanent <Saul.rathbos@gmail.com>
2025-11-02 14:22:27 +1000

    Refactored code to have DiceLoss in util
    + Added predict.py functions

---
+++ recognition/S4641797_Proj3/predict.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+from IPython.display import clear_output
+
+from util import DiceLoss
+import torchvision.transforms as transforms
+from dataset import LabeledImageDataset
+
+
+def test(model, d):
+    loss_function = DiceLoss()
+
+    transform = transforms.Compose([transforms.ToTensor(),])
+    validate = LabeledImageDataset('./data/keras_slices_data/keras_slices_train',
+                                                          './data/keras_slices_data/keras_slices_seg_train',
+                                                        transform=transform)
+    
+    overall_loss = 0
+    print(len(validate))
+    for total, img in enumerate(validate):
+        x, label = img[0].unsqueeze(0).to(d), img[1].unsqueeze(0).to(d)
+        x_hat = model(x)
+        overall_loss += loss_function(x_hat, label).item()
+        print(f"{total}/{len(validate)}")
+        clear_output(wait=True)
+        
+    print(f"test accuracy for all labels: {1 - overall_loss/total}")
+
+def disp(model, d):
+    transform = transforms.Compose([transforms.ToTensor(),])
+    validate = LabeledImageDataset('./data/keras_slices_data/keras_slices_train',
+                                                          './data/keras_slices_data/keras_slices_seg_train',
+                                                        transform=transform)
+    
+    for i in [0, 100, 1000, 10000]:
+        img = validate[i]
+        x, label = img[0].unsqueeze(0).to(d), img[1].unsqueeze(0).to(d)
+        x_hat = model(x)
+
+        fig, axs = plt.subplots(2,6)
+        axs[0][0].imshow(x.cpu().numpy()[0][0])
+        axs[0][1].imshow(label.cpu().detach().numpy()[0][0])
+        axs[1][0].imshow(x_hat.cpu().detach().numpy()[0][0])
+        axs[1][1].imshow(x_hat.cpu().detach().numpy()[0][1])
+        axs[1][2].imshow(x_hat.cpu().detach().numpy()[0][2])
+        axs[1][3].imshow(x_hat.cpu().detach().numpy()[0][3])
+        axs[1][4].imshow(x_hat.cpu().detach().numpy()[0][4])
+        axs[1][5].imshow(x_hat.cpu().detach().numpy()[0][5])
+        plt.show()
--- recognition/S4641797_Proj3/train.py
+++ recognition/S4641797_Proj3/train.py
@@ -2,39 +2,9 @@ import matplotlib.pyplot as plt
 from torch.optim import Adam
 from dataset import load_dataset
 from modules import ImprovedUNET
-import torch.nn.functional as F 
 from IPython.display import clear_output
 import torch
-
-class DiceLoss:
-    """
-    From:
-    https://medium.com/data-scientists-diary/implementation-of-dice-loss-vision-pytorch-7eef1e438f68
-    Modified to handle multi-class targets and match input dimensions.
-    """
-    def __init__(self, smooth=1):
-        self.smooth = smooth
-
-    def __call__(self, inputs, targets):
-        inputs = F.softmax(inputs, dim=1)
-
-        targets = targets.long()
-        if targets.dim() == 4:
-            targets = targets[:, 0, :, :]
-        elif targets.dim() != 3:
-            raise ValueError(f"Unsupported target shape for DiceLoss: {targets.shape}. Expected [N, 1, H, W] or [N, H, W].")
-
-        targets = F.one_hot(targets, num_classes=6) 
-
-        targets = targets.permute(0, 3, 1, 2).float()
-
-        intersection = (inputs * targets).sum(dim=(2, 3))
-        union = inputs.sum(dim=(2, 3)) + targets.sum(dim=(2, 3))
-
-        dice = (2. * intersection + self.smooth) / (union + self.smooth)
-
-        return 1 - dice.mean()
-
+from util import DiceLoss
 
 def train(EPOCHS = 50):
     batch_size = 8
